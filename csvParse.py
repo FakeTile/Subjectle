c0a574889f4308cbfe15ff46b89d600561a8fed3
FakeTile <149658591+FakeTile@users.noreply.github.com>
2025-06-25 15:20:01 +1000

    fixes ajdins mistake with the sheets

--- csvParse.py
+++ csvParse.py
@@ -11,7 +11,7 @@ with open(PATH, 'r') as f:
   print('{')
   for row in reader:
     print(f"'{row[2].split()[0]} {row[2].split()[1][:1]}': [", end='')
-    subj = row[3:11]
+    subj = row[5:13]
     for index, raw  in enumerate(subj):
       s = str(raw.split('-')[0]).strip()
       if s == "Richmond" and index == 2:
