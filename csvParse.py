fec0780a9db72e7f2541752340df6c57c75da03b
FakeTile <149658591+FakeTile@users.noreply.github.com>
2025-06-25 14:40:45 +1000

    improved parser and DL

---
+++ csvParse.py
@@ -0,0 +1,25 @@
+import csv
+
+extensionTeachers = ['Robinson', 'Black', 'Vyas', 'Treleaven', 'Penn', 'Jones']
+
+PATH = 'ppl.csv'
+
+with open(PATH, 'r') as f:
+  reader = csv.reader(f)
+  header = next(reader)
+
+  print('{')
+  for row in reader:
+    print(f"'{row[2].split()[0]} {row[2].split()[1][:1]}': [", end='')
+    subj = row[3:11]
+    for index, raw  in enumerate(subj):
+      s = str(raw.split('-')[0]).strip()
+      if s == "Richmond" and index == 2:
+        s = s + "-drama"
+      elif s == "Phelps" and 4 < index:
+        s = s + "-anc"
+      elif s in extensionTeachers and 9 > index:
+        s = s + "-ext"
+      print(f"'{s}', ", end='')
+    print('],')
+  print('}')
