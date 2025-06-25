75feb313c3e71863c1c0e9db2556f8b52af140ab
binyolk <byron.a.hastie@gmail.com>
2025-06-25 20:01:16 +1000

    Update game.js
    win alert change

--- game.js
+++ game.js
@@ -158,7 +158,7 @@ async function enterGuess(name) {
   
   await sleep(1000);
   if (name === guesee) {
-    alert(`You won in ${num_guesses} attempts`);
+    alert(`You won in ${num_guesses}`);
     return;
   }
   if (num_guesses === 6) {
