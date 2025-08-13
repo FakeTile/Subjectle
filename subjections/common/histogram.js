8c783fba57915b059b464931070e31f0e2e8ac87
binyolk <byron.a.hastie@gmail.com>
2025-08-14 09:58:34 +1000

    histogram fix and neil

--- subjections/common/histogram.js
+++ subjections/common/histogram.js
@@ -6,7 +6,7 @@ function renderHistogram(targetId) {
 
   const maxVal = Math.max(...data, 1);
 
-  for (let i = 1; i <= 5; i++) {
+  for (let i = 5; i >= 1; i--) {
     const row = document.createElement('div');
     row.className = 'histogram-row';
 
